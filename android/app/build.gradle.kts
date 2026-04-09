164ad54ee530b33beb242d8c32d2ea35ab87436d
Jean-Pierre PLINGUIER <jean-pierre.plinguier@moulinsbourgeois.com>
2026-04-09 18:42:43 +0200

    fix namespace quotes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = com.example.splitmates
+    namespace = "com.example.splitmates"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
